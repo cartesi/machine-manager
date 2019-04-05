d057fff5c9afe7fd8e2f9db9596a6df0e056b74c
Carlo Fragni <carlo@cartesi.io>
2019-04-05 20:06:09 -0300

    Bump core submodule
    Also naming the container instance in the ephemeral container script so it is easier to follow the README tutorial

--- execute_core_manager_ephemeral_container.sh
+++ execute_core_manager_ephemeral_container.sh
@@ -1,1 +1,1 @@
-docker run -it -p 127.0.0.1:50051:50051 -v `pwd`/test-files:/root/host --rm cartesi/image-core-manager
+docker run -it --name ephemeral-core-manager -p 127.0.0.1:50051:50051 -v `pwd`/test-files:/root/host --rm cartesi/image-core-manager
